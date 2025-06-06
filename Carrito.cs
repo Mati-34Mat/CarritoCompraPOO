ac46334128d17b8f0184414f15290d1f578ea239
Galo Novoa <galocnovoa@gmail.com>
2025-06-06 15:29:22 -0300

    Add files via upload

---
+++ Carrito.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarritoCompras
+{
+    class Carrito
+    {
+        public List<ItemCarrito> Productos { get; set; } = new List<ItemCarrito>();
+
+        public Carrito(List<ItemCarrito> productos) {
+            Productos = productos;
+        }
+    }
+}
