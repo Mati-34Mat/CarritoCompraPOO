8ab1444f9280a099f59a33247e2ef706aedb662e
Matías <matiasberardo07@gmail.com>
2025-06-10 09:49:59 -0300

    AgregarProductos duplicado

--- CarritoCompras/ItemCarrito.cs
+++ CarritoCompras/ItemCarrito.cs
@@ -17,25 +17,7 @@ namespace CarritoCompras
             Producto = producto;
         }
 
-        public void AgregarProducto(int codigo, int cant)
-        {
-            var existeEnTienda = Tienda.Productos.FirstOrDefault(p => p.Codigo == codigo);
-            if (existeEnTienda == null)
-            {
-                Console.WriteLine("No existe el producto.");
-                return;
-            }
-
-            var existeEnCarrito = Productos.FirstOrDefault(p => p.Codigo == codigo);
-            if(existeEnCarrito != null) 
-            { 
-                existeEnCarrito.Cantidad += cantidad;
-            }
-            else
-            {
-                Productos.Add(existeEnTienda);
-            }
-        }
+        
 
     }
 }
